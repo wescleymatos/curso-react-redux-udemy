ca4eda42371f0d8d80200271e636aeacfa9f03de
Wescley Matos <wescleymatos@gmail.com>
2017-12-12 13:47:09 -0300

    Commit inicial

---
+++ todo-app/backend/src/config/routes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+
+module.exports = (server) => {
+  //API router
+  const router = express.Router();
+  server.use('/api', router);
+
+  const todoService = require('../api/todo/todoService');
+  todoService.register(router, '/todos');
+}
---
+++ todo-app/backend/src/config/server.js
@@ -0,0 +1,12 @@
+const port = 3003;
+
+const bodyParser = require('body-parser');
+const express = require('express');
+const server = express();
+
+server.use(bodyParser.urlencoded({extended: true}));
+server.use(bodyParser.json());
+
+server.listen(port, () => console.log(`Server running on port ${port}`));
+
+module.exports = server;
