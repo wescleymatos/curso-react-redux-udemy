c26dd1a91bbf4991692382809b6e36f5e16effe0
Wescley Matos <wescleymatos@gmail.com>
2017-12-19 06:57:11 -0300

    Correção do path do css de font

--- todo-app/frontend/src/main/app.jsx
+++ todo-app/frontend/src/main/app.jsx
@@ -1,5 +1,5 @@
 import 'modules/bootstrap/dist/css/bootstrap.min.css';
-import 'modules/font-awesome/dist/css/font-awesome.min.css';
+import 'modules/font-awesome/css/font-awesome.min.css';
 
 import React from 'react';
 
