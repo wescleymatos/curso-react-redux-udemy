19d788a4d3e628618c6ed9a9945ab64d29fea54c
Wescley Matos <wescleymatos@gmail.com>
2017-12-28 12:42:37 -0300

    Criação de componente para realizar rederização condicional

--- todo-app/frontend/src/template/iconButton.jsx
+++ todo-app/frontend/src/template/iconButton.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
+import If from './if';
+
 export default props => {
-  if (props.hide) {
-    return null;
-  } else {
-    return (
+  return (
+    <If test={!props.hide}>
       <button className={'btb btn-' + props.style} onClick={props.onClick}>
         <i className={'fa fa-' + props.icon}></i>
       </button>
-    );
-  }
+    </If>
+  );
 };
