5ede054f25185f6a62fe76b6f870bb43a4a3e914
Wescley Matos <wescleymatos@gmail.com>
2018-02-20 13:43:16 -0300

    Utilização do redux thunk para disparar eventos em sequência

--- todo-app/frontend/src/todo/todoActions.js
+++ todo-app/frontend/src/todo/todoActions.js
@@ -16,11 +16,10 @@ export const search = () => {
   };
 };
 
-export const add = () => {
-  const request = axios.post(URL, { description });
-
-  return {
-    type: 'TODO_ADDED',
-    payload: request
+export const add = (description) => {
+  return dispatch => {
+    axios.post(URL, { description })
+      .then(result => dispatch({ type: 'TODO_ADDED', payload: result.data }))
+      .then(() => dispatch(search()));
   };
-}
+};
