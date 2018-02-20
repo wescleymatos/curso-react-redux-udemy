7fed553011666b9639204f1c1ca103d58a657488
Wescley Matos <wescleymatos@gmail.com>
2018-02-20 17:32:32 -0300

    Implementação do método clear

--- todo-app/frontend/src/todo/todoActions.js
+++ todo-app/frontend/src/todo/todoActions.js
@@ -7,8 +7,9 @@ export const changeDescription = event => ({
   payload: event.target.value
 });
 
-export const search = () => {
-  const request = axios.get(`${URL}?sort=-createdAt`);
+export const search = (description = '') => {
+  const search = description ? `&description__regex=/${description}/` : '';
+  const request = axios.get(`${URL}?sort=-createdAt${search}`);
 
   return {
     type: 'TODO_SEARCHED',
@@ -19,7 +20,7 @@ export const search = () => {
 export const add = description => {
   return dispatch => {
     axios.post(URL, { description })
-      .then(result => dispatch({ type: 'TODO_ADDED', payload: result.data }))
+      .then(result => dispatch(clear()))
       .then(() => dispatch(search()));
   };
 };
@@ -43,4 +44,8 @@ export const remove = todo => {
     axios.delete(`${URL}/${todo._id}`)
       .then(() => dispatch(search()));
   };
+};
+
+export const clear = () => {
+  return { type: 'TODO_CLEAR' };
 };
--- todo-app/frontend/src/todo/todoForm.jsx
+++ todo-app/frontend/src/todo/todoForm.jsx
@@ -4,7 +4,7 @@ import { bindActionCreators } from 'redux';
 
 import Grid from '../template/grid';
 import IconButton from '../template/iconButton';
-import { changeDescription, search, add } from './todoActions';
+import { changeDescription, search, add, clear } from './todoActions';
 
 class TodoForm extends Component {
   constructor(props) {
@@ -19,9 +19,9 @@ class TodoForm extends Component {
 
   keyHandler(e) {
     if (e.key === 'Enter') {
-      e.shiftKey ? this.props.search() : this.props.add(this.props.description);
+      e.shiftKey ? this.props.search(this.props.description) : this.props.add(this.props.description);
     } else if (e.key === 'Escape') {
-      props.handleClear();
+      this.props.clear();
     }
   }
 
@@ -35,8 +35,8 @@ class TodoForm extends Component {
         </Grid>
         <Grid cols='12 9 10'>
           <IconButton style='primary' icon='plus' onClick={() => this.props.add(this.props.description)} />
-          <IconButton style='info' icon='search' onClick={this.props.search} />
-          <IconButton style='default' icon='close' onClick={this.props.handleClear} />
+          <IconButton style='info' icon='search' onClick={() => this.props.search(this.props.description)} />
+          <IconButton style='default' icon='close' onClick={this.props.clear} />
         </Grid>
       </div>
     );
@@ -44,5 +44,5 @@ class TodoForm extends Component {
 }
 
 const mapStateToProps = state => ({description: state.todo.description});
-const mapDispatchToProps = dispatch => bindActionCreators({ changeDescription, search, add }, dispatch);
+const mapDispatchToProps = dispatch => bindActionCreators({ changeDescription, search, add, clear }, dispatch);
 export default connect(mapStateToProps, mapDispatchToProps)(TodoForm);
